3c6981afdc05e69c9695f11a34b8a0d957a17031
lwoncheol <46828314+lwoncheol@users.noreply.github.com>
2022-08-18 22:29:24 -0400

    Add files via upload

---
+++ BN_bulk_finite_Q/2-bgw/4-abs/01-link-files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ln -sf ../../1-mf/4-wfn_co_full/wfn.cplx WFN_co
+ln -sf ../../1-mf/5-wfn-fi-64-Q0/wfn.cplx WFN_fi
+ln -sf ../../1-mf/6-wfnq-fi-64-Q0/wfn.cplx WFNq_fi
+ln -sf ../3-ker-Q0/bsemat.h5 .
+ln -sf ../1-eps/q0/eps0mat.h5 .
+ln -sf ../1-eps/epsmat.h5 .
+ln -sf ../2-sig-full/eqp1.dat eqp_co.dat
+
